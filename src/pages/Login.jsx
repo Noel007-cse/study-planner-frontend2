ea1a580bff678f0edcc2b78c86518fafe94bdcc7
Noel J Cherian <noeljcherian07@gmail.com>
2025-08-03 12:36:37 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     setIsLoading(true);
     
     try {
-      const res = await axios.post('/api/auth/login', {
+      const res = await axios.post('https://study-planner-backend-prss.onrender.com/api/auth/login', {
         email,
         password
       });
